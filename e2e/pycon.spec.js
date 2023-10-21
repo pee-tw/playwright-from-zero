48169ceb1571c24aa54b992d0e15426e95929ccc
Pee Tankulrat <76930028+pee-tw@users.noreply.github.com>
2023-10-21 16:59:42 +0700

    Testing PyCon

---
+++ e2e/pycon.spec.js
@@ -0,0 +1,32 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test("has title", async ({ page }) => {
+  await page.goto("https://th.pycon.org/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/PyCon Thailand 2023/);
+});
+
+test("should see Buy Ticket", async ({ page }) => {
+  await page.goto("https://th.pycon.org/");
+
+  await expect(page.getByRole("button", { name: "Buy Ticket" })).toBeVisible();
+});
+
+test("should have Pee and Bumbim", async ({ page }) => {
+  await page.goto("https://th.pycon.org/");
+
+  await expect(page.getByText("Pee", { exact: true })).toBeVisible();
+  await expect(page.getByText("Bumbim, Siriwimon")).toBeVisible();
+});
+
+test("should have snake and elephant", async ({ page }) => {
+  await page.goto("https://th.pycon.org/");
+
+  const snakeIcon = page.getByText("🐍");
+  await expect(snakeIcon).toBeVisible();
+
+  await page.locator('//*[@id="root"]/div/div/div[2]/footer/div[3]').hover();
+  await expect(page.getByText("🐘")).toBeVisible();
+});
